04720321ebc6aef02c3109854ba6f2c42fbfce90
FredFerri <ferri.frederic@yahoo.fr>
2024-10-14 11:12:42 +0200

    Video block fix bug

--- assets/js/builder-admin_video.js
+++ assets/js/builder-admin_video.js
@@ -1,8 +1,8 @@
 function admin_video_scripts() {
     // Gérer l'affichage/masquage des options du bouton
-    jQuery(document).on('change', '.galigeo-builder-video .show_button_checkbox', function() {
+    jQuery(document).on('change', '.builder-block[data-type="video"] .show_button_checkbox', function() {
         var index = jQuery(this).data('index');
-        var buttonOptions = jQuery('.galigeo-builder-video .button-options[data-index="' + index + '"]');
+        var buttonOptions = jQuery('.builder-block[data-type="video"] .button-options[data-index="' + index + '"]');
         if (this.checked) {
             buttonOptions.removeClass('hidden').addClass('block');
         } else {
